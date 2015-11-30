0519f81b0731ba53a43dd4f2ba9fdfbfacf0f2a4
amboeradoel <nanangdwiprawoto@gmail.com>
2015-11-30 20:26:35 +0700

    Create trying.js

---
+++ trying.js
@@ -0,0 +1,103 @@
+var s=[
+"2.html",
+"http://faucet.bitcoinbarry.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://hentenaar.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"https://coinsteller.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://earnbitcoinonline.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://razercrypt.com/btc/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://bluesatoshi.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://btc4you.net/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://mybestbitcoin.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.breaktheco.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.21bitcoingratis.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://faucet.jokertimes.co/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.safebitco.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.theearner.co.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.faucet.bitcoins43.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://bullfaucet.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://robertfamilia.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://nextfaucet.info/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://btc4me.xyz/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://milli.io/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.bitcoin-data.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.thanksbitcoin.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://faucetbit.eu/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://rainingfreebitcoins.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://freesatoshi.info/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://btc-free.eu/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"https://bitcoinbarry.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://satoshifiesta.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://freebitcoinhome.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.faucetbitcoins.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://bitcoindrips.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.prizebitcoins.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.siizo.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://minions-faucet.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://btc-snack.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://oneadfaucet.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://your-bit.co.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://bitcoin.cryptorials.io/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://zoobit.co.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.luckybitfaucet.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://rapidbitco.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://itsrainingbitcoins.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://freesatoshi.com.ua/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://freedombit.co.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://rekbitcoin.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://faucet-bitco.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://bubble-bit.co.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.btcon.net/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.faucetfreebitcoin.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.bitcoinerz.eu/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.pickupcoin.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.satoshifaucet.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://bitcoin.xyz/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.satoshiismoney.eu/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://ptcnote.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://www.gte-free-bitco.in/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://thefreebitcoin.website/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://freshcoin.ru/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://valley-bitcoin.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk",
+"http://dorifaucet.com/?r=1Br6Hx26jSRNrmckwBU58uRXtjPqtbqnPk"
+];
+var adr,i,x=0,c=s.length;
+function address() {
+adr=prompt('Enter your bitcoin address:');
+s=s.map(function(x){
+  return x.indexOf("a=") != -1 ? x + adr : x; 
+});
+}
+function next(){
+x+=1;
+if (x>c-1)
+  {
+  x=0;
+  }
+changeSrc();
+}
+function prev(){
+x-=1;
+if (x<=0)
+  {
+  x=c-1;
+  }
+changeSrc();
+}
+function jumpTo() {
+i=prompt("Enter a number to skip to that faucet.");
+x=0;
+while (i!=x) {
+  if (x>c-2) {
+  break;
+  }
+  x+=1;
+}
+changeSrc();
+}
+function newTab() {
+var win=window.open(document.getElementById("fm").src, '_blank');
+win.focus();
+}
+function changeSrc() {
+document.getElementById("fm").src=s[x];
+}
